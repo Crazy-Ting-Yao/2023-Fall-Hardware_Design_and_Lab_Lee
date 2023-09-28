115e8621d5ccdf1e515ec2dbf933c7ae30bc8c4d
瘋狂挺腰獸 <stu111062109@gapp.nthu.edu.tw>
2023-09-28 18:41:54 +0800

    9/28 Second Commit

--- Lab1/Advanced/1-3fpga/Lab1_111062109_Crossbar_2x2_4bit_fpga.v
+++ Lab1/Advanced/1-3fpga/Lab1_111062109_Crossbar_2x2_4bit_fpga.v
@@ -50,29 +50,29 @@ module Mux_2x1_4bit(a, b, sel, f);
 
 endmodule
 
-module Crossbar_2x2_4bit(in1, in2, control, fout1, fout2);
-input [4-1:0] in1, in2;
+module Crossbar_2x2_4bit_fpga(SWITCH1, SWITCH2, control, LED1, LED2);
+input [4-1:0] SWITCH1, SWITCH2;
 input control;
-output [8-1:0] fout1, fout2;
+output [8-1:0] LED1, LED2;
 wire [4-1:0] out1, out2;
 wire [3:0] temp1, temp2, temp3, temp4;
 wire ncontrol;
 
 not notcontrol(ncontrol, control);
-Dmux_1x2_4bit d1(.in(in1), .a(temp1), .b(temp2), .sel(control));
-Dmux_1x2_4bit d2(.in(in2), .a(temp3), .b(temp4), .sel(ncontrol));
+Dmux_1x2_4bit d1(.in(SWITCH1), .a(temp1), .b(temp2), .sel(control));
+Dmux_1x2_4bit d2(.in(SWITCH2), .a(temp3), .b(temp4), .sel(ncontrol));
 
 Mux_2x1_4bit m1(.a(temp1), .b(temp3), .sel(control), .f(out1));
 Mux_2x1_4bit m2(.a(temp2), .b(temp4), .sel(ncontrol), .f(out2));
 
-fanout_1x2 f1(out1[0], fout1[0], fout1[1]);
-fanout_1x2 f2(out1[1], fout1[2], fout1[3]);
-fanout_1x2 f3(out1[2], fout1[4], fout1[5]);
-fanout_1x2 f4(out1[3], fout1[6], fout1[7]);
-fanout_1x2 f5(out2[0], fout2[0], fout2[1]);
-fanout_1x2 f6(out2[1], fout2[2], fout2[3]);
-fanout_1x2 f7(out2[2], fout2[4], fout2[5]);
-fanout_1x2 f8(out2[3], fout2[6], fout2[7]);
+fanout_1x2 f1(out1[0], LED1[0], LED1[1]);
+fanout_1x2 f2(out1[1], LED1[2], LED1[3]);
+fanout_1x2 f3(out1[2], LED1[4], LED1[5]);
+fanout_1x2 f4(out1[3], LED1[6], LED1[7]);
+fanout_1x2 f5(out2[0], LED2[0], LED2[1]);
+fanout_1x2 f6(out2[1], LED2[2], LED2[3]);
+fanout_1x2 f7(out2[2], LED2[4], LED2[5]);
+fanout_1x2 f8(out2[3], LED2[6], LED2[7]);
 
 
 endmodule
