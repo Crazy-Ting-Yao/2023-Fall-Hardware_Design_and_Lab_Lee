99a4ffcd41e8a7d4dbe2f0e08e91e4bf814d99bb
瘋狂挺腰獸 <s111062109@m111.nthu.edu.tw>
2023-11-17 13:44:35 +0800

    Upload Advanced Lab5-3 and remove Diagrams

---
+++ Lab5/Advanced/5-3/Lab5_111062109_Greatest_Common_Divisor.v
@@ -0,0 +1,78 @@
+`timescale 1ns/1ps
+
+module Greatest_Common_Divisor (clk, rst_n, start, a, b, done, gcd);
+    input clk, rst_n;
+    input start;
+    input [15:0] a;
+    input [15:0] b;
+    output done;
+    output [15:0] gcd;
+
+    parameter WAIT = 2'b00;
+    parameter CAL = 2'b01;
+    parameter FINISH = 2'b10;
+
+    reg [1:0] state, next_state;
+    reg [15:0] a_tmp, b_tmp;
+    reg [15:0] next_a, next_b;
+
+    reg delay_one_cycle;
+    always @(posedge clk) begin
+        if(!rst_n)begin
+            state <= WAIT;
+            delay_one_cycle <= 0;
+        end
+        else begin
+            state <= next_state;
+            if(next_state==FINISH) delay_one_cycle <= ~delay_one_cycle;
+            else delay_one_cycle <= 0;
+        end
+        a_tmp <= next_a;
+        b_tmp <= next_b;
+    end
+
+    always @(*) begin
+        case(state)
+        WAIT: begin
+            if(start) next_state = CAL;
+            else next_state = WAIT;
+        end
+        CAL: begin
+            if(a_tmp==b_tmp) next_state = FINISH;
+            else next_state = CAL;
+        end
+        FINISH: begin
+            if(delay_one_cycle) next_state = FINISH;
+            else next_state = WAIT;
+        end
+        default: begin
+            next_state = WAIT;
+        end
+        endcase
+    end
+
+    always @(*) begin
+        case(state)
+        WAIT: begin
+            next_a = a;
+            next_b = b;
+        end
+        CAL: begin
+            if(a_tmp>b_tmp) begin
+                next_a = a_tmp - b_tmp;
+                next_b = b_tmp;
+            end
+            else begin
+                next_a = a_tmp;
+                next_b = b_tmp - a_tmp;
+            end
+        end
+        FINISH: begin
+            next_a = a_tmp;
+            next_b = b_tmp;
+        end
+        endcase
+    end
+    assign done = (state==FINISH);
+    assign gcd = (state==FINISH) ? a_tmp : 0;
+endmodule
---
+++ Lab5/Advanced/5-3/Lab5_111062109_Greatest_Common_Divisor_t.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+`include "Lab5_111062109_Greatest_Common_Divisor.v"
+
+module Greatest_Common_Divisor_t;
+    reg clk, rst_n, start;
+    reg [15:0] a, b;
+    wire done;
+    wire [15:0] gcd;
+
+    Greatest_Common_Divisor dut(clk, rst_n, start, a, b, done, gcd);
+
+    parameter cyc = 10;
+
+    initial begin
+        $dumpfile("Greatest_Common_Divisor.vcd");
+        $dumpvars(0, Greatest_Common_Divisor_t);
+        clk = 0;
+        rst_n = 0;
+        start = 0;
+        #cyc rst_n = 1;
+        #cyc start = 1;
+        a = 15'd18;
+        b = 15'd12;
+        #cyc a = 15'd12;
+        start = 0;
+        #(20*cyc) a = 15'd1000;
+        #cyc b = 15'd160;
+        start = 1;
+        #cyc start = 0;
+        #(20*cyc) $finish;
+    end
+
+    always #5 clk = ~clk;
+endmodule
