b4db5878653b6249d968aaa6129b7bf680c411b3
瘋狂挺腰獸 <s111062109@m111.nthu.edu.tw>
2023-12-13 15:54:37 +0800

    Update motor.v

--- Lab6/Advanced/car_ver3/motor.v
+++ Lab6/Advanced/car_ver3/motor.v
@@ -5,25 +5,47 @@ module motor(
     output [1:0] pwm,
     output [1:0] dir // 1: backward, 0: forward
 );
-    reg signed [9:0] next_left_motor, next_right_motor;
-    reg signed [9:0] left_motor, right_motor;
+    wire signed [9:0] left_motor, right_motor;
     wire left_pwm, right_pwm;
-    wire [11:0] period = 32'd4000;
+    parameter period = 12'd4000;
     reg [11:0] count;
     wire [9:0] left_motor_abs, right_motor_abs;
+    wire renew = (count + 1 == period) ? 1'b1 : 1'b0;
 
+    duty_gen dg(clk, rst, renew, mode, left_motor, right_motor);
     assign left_motor_abs = {1'b1, (left_motor[9]) ? ~left_motor[8:0] : left_motor[8:0]};
     assign right_motor_abs = {1'b1, (right_motor[9]) ? ~right_motor[8:0] : right_motor[8:0]};
-
     assign dir = {left_motor[9], right_motor[9]};
-
-    PWM_gen pg0(clk, rst, left_motor_abs, count, left_pwm);
-    PWM_gen pg1(clk, rst, right_motor_abs, count, right_pwm);
+    PWM_gen pg0(clk, left_motor_abs, count, left_pwm);
+    PWM_gen pg1(clk, right_motor_abs, count, right_pwm);
     
     always @(posedge clk, posedge rst) begin
-        if (rst) count <= 32'b0;
-        else count <= (count + 1 == period) ? 0 : count + 32'd1;
+        if (rst) count <= 12'b0;
+        else count <= renew ? 0 : count + 12'd1;
     end
+    
+    assign pwm = {left_pwm, right_pwm};
+endmodule
+
+//generte PWM by input frequency & duty
+module PWM_gen (
+    input wire clk,
+    input [9:0] duty,
+    input [11:0] count,
+    output reg PWM
+);
+    parameter period = 12'd4000;
+    wire [11:0] count_duty = period * duty / 12'd1024;
+    always @(posedge clk) begin
+        PWM <= (count < count_duty) ? 1'b1 : 1'b0;
+    end
+endmodule
+
+module duty_gen(clk, rst, renew, mode, left_motor, right_motor);
+    input clk, rst, renew;
+    input [2:0] mode;
+    output reg signed [9:0] left_motor, right_motor;
+    reg signed [9:0] next_left_motor, next_right_motor;
     parameter signed full_speed = 10'sd511;
     parameter signed minimum_speed = -10'sd300;
     parameter signed increment = 10'sd50;
@@ -61,13 +83,12 @@ module motor(
             end
         endcase
     end
-
-    always @(posedge clk, posedge rst)begin
+    always @(posedge clk)begin
         if (rst) begin
             left_motor  <= full_speed;
             right_motor <= full_speed;
         end 
-        else if (count == 0) begin
+        else if (renew) begin
             left_motor  <= next_left_motor;
             right_motor <= next_right_motor;
         end
@@ -76,22 +97,4 @@ module motor(
             right_motor <= right_motor;
         end
     end
-    assign pwm = {left_pwm, right_pwm};
 endmodule
-
-//generte PWM by input frequency & duty
-module PWM_gen (
-    input wire clk,
-    input wire reset,
-    input [9:0] duty,
-    input [11:0] count,
-    output reg PWM
-);
-    wire [11:0] count_duty = 32'd4000 * duty / 32'd1024;
-    always @(posedge clk, posedge reset) begin
-        if (reset) PWM <= 1'b0;
-        else if(count < count_duty) PWM <= 1'b1;
-        else PWM <= 1'b0;
-    end
-endmodule
-
